8eed18d28e693a38943642bcc673ed6425ed6865
kiaeh2323 <kiaeh2323@naver.com>
2022-09-22 18:55:02 +0900

    feat: Customer Management program
    java extends practice

---
+++ java/src/me/java/ExtendsPractice/CustomerTest.java
@@ -0,0 +1,23 @@
+package me.java.ExtendsPractice;
+
+public class CustomerTest {
+
+    public static void main(String args[]){
+
+        Customer customerLee = new Customer();
+
+        customerLee.setCustomerName("이순신");
+        customerLee.setCustomerID(10010);
+        customerLee.setBonusPoint(1000);
+
+        System.out.println(customerLee.showCustomerInfo());
+
+        VIPCustomer customerKim = new VIPCustomer();
+
+        customerKim.setCustomerName("김유신");
+        customerKim.setCustomerID(10000);
+        customerKim.setBonusPoint(10000);
+
+        System.out.println(customerKim.showCustomerInfo());
+    }
+}
---
+++ java/src/me/java/ExtendsPractice/VIPCustomer.java
@@ -0,0 +1,29 @@
+package me.java.ExtendsPractice;
+
+public class VIPCustomer extends Customer {
+    double saleRatio;
+    String agentID;
+
+    public VIPCustomer(){
+
+        bonusRatio = 0.05;
+        saleRatio = 0.1;
+        customerGrade = "VIP";
+    }
+
+    public double getSaleRatio() {
+        return saleRatio;
+    }
+
+    public void setSaleRatio(double saleRatio) {
+        this.saleRatio = saleRatio;
+    }
+
+    public String getAgentID() {
+        return agentID;
+    }
+
+    public void setAgentID(String agentID) {
+        this.agentID = agentID;
+    }
+}
