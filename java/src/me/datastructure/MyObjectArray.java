153b2d9906857a41b5c166bd6907b23dd733dd61
kiaeh2323 <kiaeh2323@naver.com>
2022-09-27 16:57:50 +0900

    Done implement MyArray (Array Practice)

---
+++ java/src/me/datastructure/MyObjectArray.java
@@ -0,0 +1,20 @@
+package me.datastructure;
+
+public class MyObjectArray {
+    private int cout;
+    private Object[] array;
+    public int ARRAY_SIZE;
+
+    public MyObjectArray()
+    {
+        ARRAY_SIZE = 10;
+        array = new Object[ARRAY_SIZE];
+    }
+
+    public MyObjectArray(int size)
+    {
+        ARRAY_SIZE = size;
+        array = new Object[ARRAY_SIZE];
+    }
+
+}
